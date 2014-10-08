929bbb37b15e0ad41fc7f05914c988561cb00810
Kevin Mulvey <king.feruke@gmail.com>
2014-10-08 07:37:40 -0400

    start tests

--- tests/rules/bs-text-align.js
+++ tests/rules/bs-text-align.js
@@ -7,12 +7,13 @@
         name: "Bootstrap Float Errors",
 
         "Using floats with bootsrap should result in a warning": function(){
-            var result = CSSLint.verify(".foo { float: right; float: left; }", { "bs-floats": 1 });
-            Assert.areEqual(2, result.messages.length);
+            var result = CSSLint.verify(".foo { text-align: left; text-align: center; text-align: right; text-align: justify; text-align: nowrap;}", { "bs-text-align": 1 });
+            
+						Assert.areEqual(5, result.messages.length);
             Assert.areEqual("warning", result.messages[0].type);
-            Assert.areEqual("Use .pull-right with bootstrap.", result.messages[0].message);
+            Assert.areEqual("Use .text-left with bootstrap.", result.messages[0].message);
             Assert.areEqual("warning", result.messages[1].type);
-            Assert.areEqual("Use .pull-left with bootstrap.", result.messages[1].message);
+            Assert.areEqual("Use .pull-center with bootstrap.", result.messages[1].message);
         }
     }));
 })();

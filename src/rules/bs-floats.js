927a17ed5896a7a5c8650b668ceddc9a8b7609a6
Kevin Mulvey <king.feruke@gmail.com>
2014-10-07 20:08:09 -0400

    add check for floats

---
+++ src/rules/bs-floats.js
@@ -0,0 +1,34 @@
+/*
+ * Rule: box-sizing doesn't work in IE6 and IE7.
+ */
+
+CSSLint.addRule({
+
+    //rule information
+    id: "bs-floats",
+    name: "Use .pull-left and .pull-right",
+    desc: "Bootstrap already provides float util classes.",
+    browsers: "All",
+    tags: ["Bootstrap"],
+
+    //initialization
+    init: function(parser, reporter){
+        "use strict";
+        var rule = this;
+
+        parser.addListener("property", function(event){
+            var name = event.property.text.toLowerCase();
+						
+            if (name === "float"){
+                var value = event.value.text.toLowerCase();
+								if(value === "left"){
+								    reporter.report("Use .pull-left.", event.line, event.col, rule);
+                }
+								else if(value === "right"){
+								    reporter.report("Use .pull-right.", event.line, event.col, rule);
+                }
+						}
+        });
+    }
+
+});

a2f7b105a37dbf349f703b59c3f4dc1a4ed2575d
Kevin Mulvey <king.feruke@gmail.com>
2014-10-08 07:30:29 -0400

    add all values

--- src/rules/bs-text-align.js
+++ src/rules/bs-text-align.js
@@ -19,13 +19,22 @@ CSSLint.addRule({
         parser.addListener("property", function(event){
             var name = event.property.text.toLowerCase();
 						
-            if (name === "float"){
+            if (name === "text-align"){
                 var value = event.value.text.toLowerCase();
 								if(value === "left"){
-								    reporter.report("Use .pull-left with bootstrap.", event.line, event.col, rule);
+								    reporter.report("Use .text-left with bootstrap.", event.line, event.col, rule);
                 }
 								else if(value === "right"){
-								    reporter.report("Use .pull-right with bootstrap.", event.line, event.col, rule);
+								    reporter.report("Use .text-right with bootstrap.", event.line, event.col, rule);
+                }
+								else if(value === "center"){
+								    reporter.report("Use .text-center with bootstrap.", event.line, event.col, rule);
+                }
+								else if(value === "justify"){
+								    reporter.report("Use .text-justify with bootstrap.", event.line, event.col, rule);
+                }
+								else if(value === "nowrap"){
+								    reporter.report("Use .text-nowrap with bootstrap.", event.line, event.col, rule);
                 }
 						}
         });
